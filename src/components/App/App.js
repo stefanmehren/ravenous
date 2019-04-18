157ff26627d71592c4a60f65b277a1f5d44d0280
Mehren <stefan.mehren@schott-music.com>
2019-04-18 16:50:35 +0200

    Erste Hälfte der zweiten Projektphase implementiert

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,19 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+ name: 'MarginOtto Pizzeria',
+ address: '1010 Paddington Way',
+ city: 'Flavortown',
+ state: 'NY',
+ zipCode: '10101',
+ category: 'Italian',
+ rating: 4.5,
+ reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business]
 
 class App extends Component {
   render() {
@@ -11,7 +24,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
